8c01779b6f7bb2eed32d1fed23c86e5beccca8a3
Damian Reyes <reyesmezadamian@gmail.com>
2019-11-25 23:40:15 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,53 @@
+def using_push (array, string)
+    array.push(string)
+end
+
+def using_unshift (array, string)
+    array.unshift(string)
+end
+
+def using_pop (list)
+  list.pop
+end
+
+def pop_with_args (list)
+  list.pop(2)
+end
+
+def using_shift (list)
+  list.shift
+end 
+
+def shift_with_args (list)
+  list.shift(2)
+end 
+
+def using_concat (array, string)
+  array.concat (string)
+end
+
+def using_insert (list, array)
+  list.insert(4, array)
+end
+
+def using_uniq (array)
+  array.uniq
+end
+
+def using_flatten (array)
+  array.flatten
+end
+
+def using_delete (array, string)
+  array.delete(string)
+end
+
+def using_delete_at (array, list)
+  array = ["Robocop"]
+ array.delete_at(0)
+end
+
+
+
+
+
